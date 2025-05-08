20549d7f73f710814a6a958372136bd5edb64241
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-05-08 03:10:18 -0300

    mongoDB connection

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -1,5 +1,20 @@
 package com.udemy.workshopmongo.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll(){
+		return repository.findAll();
+	}
 }
