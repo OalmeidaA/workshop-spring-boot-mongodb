a0472acd467927ad63ec10f923acbd755108000e
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-05-10 01:08:09 -0300

    POST users

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemy.workshopmongo.domain.User;
+import com.udemy.workshopmongo.dto.UserDto;
 import com.udemy.workshopmongo.repository.UserRepository;
 import com.udemy.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User (objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
