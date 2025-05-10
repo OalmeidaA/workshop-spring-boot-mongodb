4ced67b24265d3e223c7265cae8d0272c6d27a14
Henrique Alves <155191957+HenrykeAlmeida@users.noreply.github.com>
2025-05-10 17:52:38 -0300

    Delete and exception

--- src/main/java/com/udemy/workshopmongo/services/UserService.java
+++ src/main/java/com/udemy/workshopmongo/services/UserService.java
@@ -1,10 +1,13 @@
 package com.udemy.workshopmongo.services;
 
+import java.beans.Transient;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.udemy.workshopmongo.domain.User;
 import com.udemy.workshopmongo.dto.UserDto;
@@ -17,21 +20,32 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<User> findAll(){
 		return repository.findAll();
 	}
 	
+	@Transactional(readOnly = true)
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 	
+	@Transactional
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
 	
-	public User fromDto(UserDto objDto) {
-		return new User (objDto.getId(), objDto.getName(), objDto.getEmail());
+	@Transactional
+	public void delete(String id) { 
+		if(!repository.existsById(id)){ 
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		repository.deleteById(id);
 	}
-	
+
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
